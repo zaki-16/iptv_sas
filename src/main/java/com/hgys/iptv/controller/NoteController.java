0608734e5537eb0180f2595f0c88ec9bcfb59055
yance <2444864349@qq.com>
2019-05-09 16:55:24 +0800

    修改

--- src/main/java/com/hgys/iptv/controller/NoteController.java
+++ src/main/java/com/hgys/iptv/controller/NoteController.java
@@ -30,7 +30,7 @@ public class NoteController {
     private NoteService noteService;
 
     @PostMapping("/addNote")
-    @ApiOperation(value = "新增结算类型-业务定比例",notes = "返回处理结果，false或true")
+    @ApiOperation(value = "新增备注模板",notes = "返回处理结果，false或true")
     @ResponseStatus(HttpStatus.OK)
     public ResultVO<?> addNote(@ApiParam(value = "新增备注模板VM") @RequestBody() NoteAddControllerVM vm){
         return noteService.addNote(vm);
@@ -53,7 +53,7 @@ public class NoteController {
     @GetMapping("/findByConditions")
     @ApiOperation(value = "通过条件查询",notes = "返回Json数据")
     @ResponseStatus(HttpStatus.OK)
-    public ResultVO<?> findByConditions(@ApiParam(value = "结算单维度名称") @RequestParam(value = "content",required = false )String content,
+    public ResultVO<?> findByConditions(@ApiParam(value = "备注模板内容") @RequestParam(value = "content",required = false )String content,
                                                                       @ApiParam(value = "用户id(0:表示公共类型)") @RequestParam(value = "userId",required = false)String userId,
                                                                       @ApiParam(value = "类型") @RequestParam(value = "noteType",required = false)String noteType){
         Sort sort = new Sort(Sort.Direction.DESC,"create_time");
