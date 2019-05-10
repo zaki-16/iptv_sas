8ca1fe7bd2e1dd734729c0248444f6cc84bd78bb
yance <2444864349@qq.com>
2019-05-10 15:30:24 +0800

    修改JPAQueryFactory注入

--- src/main/java/com/hgys/iptv/IptvSasApplication.java
+++ src/main/java/com/hgys/iptv/IptvSasApplication.java
@@ -1,10 +1,15 @@
 package com.hgys.iptv;
 
 import com.hgys.iptv.common.VerifyServlet;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
         (exclude={
@@ -22,5 +27,16 @@ public class IptvSasApplication {
         ServletRegistrationBean registration = new ServletRegistrationBean(new VerifyServlet());
         registration.addUrlMappings("/getVerifyCode");
         return registration;
+    }
+
+    /**
+     * 注入JPAQueryFactory
+     * @param entityManager
+     * @return
+     */
+    @Bean
+    @Autowired
+    public JPAQueryFactory jpaQuery(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
     }
 }
