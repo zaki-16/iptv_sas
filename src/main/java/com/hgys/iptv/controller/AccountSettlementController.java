652b078467fea160c74a23361b225bbd6812e19c
yance <2444864349@qq.com>
2019-05-13 19:58:11 +0800

    新增分配结算新增demo

--- src/main/java/com/hgys/iptv/controller/AccountSettlementController.java
+++ src/main/java/com/hgys/iptv/controller/AccountSettlementController.java
@@ -1,11 +1,49 @@
 package com.hgys.iptv.controller;
 
+import com.hgys.iptv.controller.vm.AccountSettlementAddVM;
+import com.hgys.iptv.model.vo.ResultVO;
+import com.hgys.iptv.service.AccountSettlementService;
+import com.hgys.iptv.util.ResultVOUtil;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.sql.Timestamp;
 
 @RestController()
 @RequestMapping("/accountSettlement")
 @Api(value = "accountSettlement",tags = "分账结算Api接口")
 public class AccountSettlementController {
+
+    @Autowired
+    private AccountSettlementService accountSettlementService;
+
+    @PostMapping(value = "addAccountSettlement")
+    @ApiOperation(value = "新增分配结算",notes = "返回处理结果，false或true")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResultVO<?> addAccountSettlement(@ApiParam(value = "上传的excel文件") MultipartFile file,
+                                            @ApiParam(value = "名称",required = true) @RequestParam(value = "name") String name,
+                                            @ApiParam(value = "结算规则编码",required = true) @RequestParam(value = "setRuleCode",required = true) String setRuleCode,
+                                            @ApiParam(value = "状态",required = true) @RequestParam(value = "status") Integer status,
+                                            @ApiParam(value = "备注") @RequestParam(value = "remakes",required = false) String remakes,
+                                            @ApiParam(value = "结算开始时间",required = true) @RequestParam(value = "setStartTime") Timestamp setStartTime,
+                                            @ApiParam(value = "结算结束时间",required = true) @RequestParam(value = "setEndTime") Timestamp setEndTime
+                                            ){
+
+        if (StringUtils.isBlank(name)){
+            return ResultVOUtil.error("1","分配结算name不能为空");
+        }
+
+        if (StringUtils.isBlank(setRuleCode)){
+            return ResultVOUtil.error("1","分配结算setRuleCode不能为空");
+        }
+
+        return accountSettlementService.addAccountSettlement();
+    }
 }
--- src/main/java/com/hgys/iptv/service/impl/AccountSettlementServiceImpl.java
+++ src/main/java/com/hgys/iptv/service/impl/AccountSettlementServiceImpl.java
@@ -1,8 +1,19 @@
 package com.hgys.iptv.service.impl;
 
+import com.hgys.iptv.model.vo.ResultVO;
 import com.hgys.iptv.service.AccountSettlementService;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class AccountSettlementServiceImpl implements AccountSettlementService {
+    /**
+     * 新增分配结算
+     * @return
+     */
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public ResultVO<?> addAccountSettlement() {
+        return null;
+    }
 }
