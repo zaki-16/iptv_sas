920c2fc65da0c6eae8b878da7a55a72e6d91f892
yance <2444864349@qq.com>
2019-05-22 20:31:17 +0800

    xiu

--- src/main/java/com/hgys/iptv/util/excel/PathConstant.java
+++ src/main/java/com/hgys/iptv/util/excel/PathConstant.java
@@ -1,6 +1,7 @@
 package com.hgys.iptv.util.excel;
 
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.ClassUtils;
 import org.springframework.util.ResourceUtils;
 
 import java.io.FileNotFoundException;
@@ -14,7 +15,8 @@ public class PathConstant {
 	 */
 	public static String getExcelExportResource() throws IOException {
 //		return ResourceUtils.getFile("classpath:excel").toString();
-		return 	new ClassPathResource("excel").getFile().getPath();
+//		return 	new ClassPathResource("excel").getFile().getPath();
+		return ClassUtils.getDefaultClassLoader().getResource("excel").getPath();
 	}
 
 
