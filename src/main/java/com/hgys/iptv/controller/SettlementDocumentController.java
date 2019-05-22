973099258f78c1d5d39a1267ee8991ba3aea9268
yance <2444864349@qq.com>
2019-05-22 10:40:38 +0800

    新增结算文档接口控制器

---
+++ src/main/java/com/hgys/iptv/controller/SettlementDocumentController.java
@@ -0,0 +1,14 @@
+package com.hgys.iptv.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yangpeng
+ */
+@RestController
+@RequestMapping("/settlementDocumentController")
+@Api(value = "settlementDocumentController",tags = "结算文档Api接口")
+public class SettlementDocumentController {
+}
