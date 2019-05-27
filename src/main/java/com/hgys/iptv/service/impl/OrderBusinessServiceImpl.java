700bf59eaff7f5a42ae659d39bf030dd133d0ae4
qi <471890699@qq.com>
2019-05-27 16:10:45 +0800

    模糊

--- src/main/java/com/hgys/iptv/service/impl/OrderBusinessServiceImpl.java
+++ src/main/java/com/hgys/iptv/service/impl/OrderBusinessServiceImpl.java
@@ -284,12 +284,12 @@ public class OrderBusinessServiceImpl implements OrderBusinessService {
             List<Predicate> predicates = new ArrayList<>();
 
             if (StringUtils.isNotBlank(name)) {
-                Predicate condition = builder.like(root.get("name"), name);
+                Predicate condition = builder.like(root.get("name"), "%"+name+"%");
                 predicates.add(condition);
             }
 
             if (StringUtils.isNotBlank(code)) {
-                Predicate condition = builder.like(root.get("code"), code);
+                Predicate condition = builder.like(root.get("code"), "%"+code+"%");
                 predicates.add(condition);
             }
 
