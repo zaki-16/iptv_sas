9903caf05d5a534aae3f41c261e2ec0ca1fdaa67
yance <2444864349@qq.com>
2019-05-27 11:49:52 +0800

    新增日志

--- src/main/java/com/hgys/iptv/controller/SettlementController.java
+++ src/main/java/com/hgys/iptv/controller/SettlementController.java
@@ -1,5 +1,6 @@
 package com.hgys.iptv.controller;
 
+import com.hgys.iptv.aop.SystemControllerLog;
 import com.hgys.iptv.model.vo.ResultVO;
 import com.hgys.iptv.service.SettlementService;
 import com.hgys.iptv.util.ResultVOUtil;
@@ -25,6 +26,7 @@ public class SettlementController {
     @PostMapping("/settlement")
     @ApiOperation(value = "分账结算结算接口",notes = "返回处理结果，false或true")
     @ResponseStatus(HttpStatus.CREATED)
+    @SystemControllerLog(target = "分账结算",methodName = "SettlementDimensionController.settlement",type = "新增")
     public ResultVO<?> settlement(@ApiParam(value = "分账结算id") @RequestParam String id){
         if (StringUtils.isBlank(id)){
             return ResultVOUtil.error("1","分账结算ID不能为空!");
@@ -35,6 +37,7 @@ public class SettlementController {
     @PostMapping("/cancel")
     @ApiOperation(value = "分账结算撤销接口",notes = "返回处理结果，false或true")
     @ResponseStatus(HttpStatus.CREATED)
+    @SystemControllerLog(target = "分账结算",methodName = "SettlementDimensionController.cancel",type = "修改")
     public ResultVO<?> cancel(@ApiParam(value = "分账结算Id") @RequestParam String id){
         if (StringUtils.isBlank(id)){
             return ResultVOUtil.error("1","分账结算ID不能为空!");
