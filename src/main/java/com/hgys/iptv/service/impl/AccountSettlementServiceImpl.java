98253819ab294b6b486506d8704cb098e2327cdb
qi <471890699@qq.com>
2019-05-28 10:47:28 +0800

    Merge branch 'tangjiqi' of http://192.168.31.193:8766/wangz/iptv_sas into master-new
    # Conflicts:
    #	src/main/java/com/hgys/iptv/service/impl/OrderCpServiceImpl.java

--- src/main/java/com/hgys/iptv/service/impl/AccountSettlementServiceImpl.java
+++ src/main/java/com/hgys/iptv/service/impl/AccountSettlementServiceImpl.java
@@ -606,7 +606,7 @@ public class AccountSettlementServiceImpl implements AccountSettlementService {
     }
 
     @Override
-    public Page<AccountSettlementAddVM> findByConditions(String name, String code, String status, Pageable pageable,String startTime,String endTime) {
+    public Page<AccountSettlementAddVM> findByConditions(String name, String code, String status, Pageable pageable,String startTime,String endTime,String set_type) {
         Page<AccountSettlementAddVM> map = accountSettlementRepository.findAll(((root, query, builder) -> {
             List<Predicate> predicates = new ArrayList<>();
 
@@ -622,6 +622,10 @@ public class AccountSettlementServiceImpl implements AccountSettlementService {
 
             if (StringUtils.isNotBlank(status)) {
                 Predicate condition = builder.equal(root.get("status"), Integer.parseInt(status));
+                predicates.add(condition);
+            }
+            if (StringUtils.isNotBlank(set_type)) {
+                Predicate condition = builder.equal(root.get("set_type"), Integer.parseInt(set_type));
                 predicates.add(condition);
             }
 
